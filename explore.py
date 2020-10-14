410d77ebd4047cecdc2c13a38b92d55bdd7a07c2
Massimiliano Galli <massimiliano.galli@cern.ch>
2020-10-14 15:08:45 +0200

    Enable multithreading

--- explore.py
+++ explore.py
@@ -4,6 +4,7 @@ def extract_all_histos_efficiently(tree, output_file):
         """
         Extract all histograms from a tree and save them in a file
         """
+    ROOT.EnableImplicitMT()
     rdf = ROOT.RDataFrame(tree)
     r_rst_ptrs = [rdf.Histo1D(column) for column in rdf.GetColumnNames()]
     f = ROOT.TFile(output_file, 'NEW')
