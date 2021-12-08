42b45bd01e7caecbac71ab4202ad9ff0ef76de14
Massimiliano Galli <massimiliano.galli@cern.ch>
2021-12-08 13:10:04 +0100

    Add roffit utils

--- roofit_utils/workspaces.py
+++ roofit_utils/workspaces.py
@@ -1,47 +1,50 @@
 import ROOT
 
+from cppyy.gbl import RooRealVar, RooFormulaVar, RooAbsPdf, RooConstVar, RooDataSet, RooDataHist
+
+
+def make_dict(obj):
+
+    return {
+        'name': obj.GetName(),
+        'instance': obj,
+        'type': obj.ClassName(),
+    }
+
+
 def ws_to_dict(ws):
     """
     Convert a workspace to a dictionary of objects.
     """
-    ws_dict = {}
+    ws_dict = {
+        "variables": [],
+        "functions": [],
+        "pdfs": [],
+        "datasets": [],
+        "histograms": [],
+        "constants": [],
+        "categories": [],
+        "other": []
+    }
 
-    # variables
-    ws_dict['variables'] = []
-    for var in ws.allVars():
-        ws_dict['variables'].append({
-            "name": var.GetName(),
-            "value": var.getVal(),
-            "error": var.getError(),
-            "range": (var.getMin(), var.getMax()),
-            }
-        )
+    objects = ws.components()
 
-    # functions
-    ws_dict['functions'] = []
-    for func in ws.allFunctions():
-        ws_dict['functions'].append({
-            "name": func.GetName(),
-            "object": func
-            }
-        )
-
-    # pdfs
-    ws_dict['pdfs'] = []
-    for pdf in ws.allPdfs():
-        ws_dict['pdfs'].append({
-            "name": pdf.GetName(),
-            "object": pdf
-            }
-        )
-
-    # datasets
-    ws_dict['datasets'] = []
-    for data in ws.allData():
-        ws_dict['datasets'].append({
-            "name": data.GetName(),
-            "object": data
-            }
-        )
+    for obj in objects:
+        if obj.InheritsFrom('RooRealVar'):
+            ws_dict['variables'].append(make_dict(obj))
+        elif obj.InheritsFrom('RooFormulaVar') or obj.InheritsFrom('RooRecursiveFraction'):
+            ws_dict['functions'].append(make_dict(obj))
+        elif obj.InheritsFrom('RooAbsPdf'):
+            ws_dict['pdfs'].append(make_dict(obj))
+        elif obj.InheritsFrom('RooDataSet'):
+            ws_dict['datasets'].append(make_dict(obj))
+        elif obj.InheritsFrom('RooDataHist'):
+            ws_dict['histograms'].append(make_dict(obj))
+        elif obj.InheritsFrom('RooConstVar'):
+            ws_dict['constants'].append(make_dict(obj))
+        elif obj.InheritsFrom('RooCategory'):
+            ws_dict['categories'].append(make_dict(obj))
+        else:
+            ws_dict['other'].append(make_dict(obj))
 
     return ws_dict
